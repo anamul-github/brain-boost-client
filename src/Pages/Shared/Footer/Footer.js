c9f0fe5d1e98650ca31fd90bab08de062220e95a
Anamul10 <anamul.hasan@mnsu.edu>
2023-02-04 23:29:05 -0500

    created some components and router set up done

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <h2 className='text-center'>This is Footer</h2>
+
         </div>
     );
 };
--- src/Pages/Shared/LeftNav/LeftNav.js
+++ src/Pages/Shared/LeftNav/LeftNav.js
@@ -3,7 +3,7 @@ import React from 'react';
 const LeftNav = () => {
     return (
         <div>
-            <h2 className='text-center py-10'>This is Left Navigation</h2>
+
         </div>
     );
 };
