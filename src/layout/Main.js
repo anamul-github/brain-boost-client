3ba0beaef63f6377268e693e9a6fcf4d032fb93c
Anamul10 <anamul.hasan@mnsu.edu>
2023-02-06 13:22:53 -0500

    background added

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Pages/Shared/Footer/Footer';
 import Header from '../Pages/Shared/Header/Header';
-import LeftNav from '../Pages/Shared/LeftNav/LeftNav';
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
-            <Container>
-                <Row>
-                    <Col lg="2" className='d-lg-block'>
-                        <LeftNav></LeftNav>
-                    </Col>
-                    <Col lg="10">
-                        <Outlet></Outlet>
-                    </Col>
-                </Row>
-            </Container>
+            <Outlet></Outlet>
             <Footer></Footer>
         </div>
     );
