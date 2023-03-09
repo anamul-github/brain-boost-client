732d1da1daf229fa30aafdb07a6e5099b6414dbb
Anamul10 <anamul.hasan@mnsu.edu>
2023-03-08 22:21:44 -0500

    added a section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 
 const Home = () => {
     return (
-        <div>
-
-        </div>
+        <section className="py-5 bg-light">
+            <Container fluid>
+                <Row className="align-items-center">
+                    <Col md={6}>
+                        <h2>Learn, Grow and Succeed with Brain Boost</h2>
+                        <p className="my-4">
+                            Brain Boost is an innovative learning platform that offers courses on a wide range of topics, from coding and design to marketing and business. With expert instructors and engaging content, you can take your skills to the next level and achieve your goals.
+                        </p>
+                        <Button variant="primary" className="mr-3">Explore Courses</Button>
+                        <Button variant="outline-primary">Sign Up Today</Button>
+                    </Col>
+                    <Col md={6}>
+                        <div className="video-placeholder"></div>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
     );
-};
+}
 
 export default Home;
+
+
